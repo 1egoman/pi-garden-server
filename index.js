4e4da49b366723061ca72f7f1e31281e0be9261f
1egoman <rsg1egoman@gmail.com>
2014-10-10 18:52:40 -0400

    initial

---
+++ index.js
@@ -0,0 +1,59 @@
+var thing = require("iot-thing");
+var strftime = require("strftime");
+
+// create the thing
+new thing("127.0.0.1" || process.env.HOST, 8000 || process.env.PORT, {}, {
+  name: "Hydroponic Garden",
+  desc: "A garden that grows me stuff",
+  tags: ["garden", "hydroponics"],
+  image: "http://panamashippingcontainerhouse.com/wp-content/uploads/2012/07/Hydroponics-650x487.jpg",
+  data: {
+    wateringTime: {
+      value: "11:00:00"
+    },
+    wateringDurationMinutes: {
+      value: 10
+    },
+    currentlyWatering: {
+      value: false,
+      type: "button",
+      readonly: true
+    },
+    turnPumpOn: {
+      label: "Run pump through a cycle",
+      value: false
+    }
+  }
+}, function(thing, err) {
+
+  var pump = require("./pump")(thing);
+  thing || console.log(err);
+
+  // the global timer
+  var globalTimer = setInterval(function() {
+    // test to see if it is time for the pump to turn on
+    thing.data.pull("wateringTime", function(when) {
+    // console.log(strftime('%T'), when)
+      if ( strftime('%T') == when.value ) {
+        thing.data.pull("wateringDurationMinutes", function(duration) {
+          pump.doCycle(duration.value, when.value);
+        });
+      }
+    });
+
+    // manually do a cycle
+    thing.data.pull("turnPumpOn", function(doCycle) {
+      if (doCycle.value) {
+        thing.data.push("turnPumpOn", false, function() {});
+
+        thing.data.pull("wateringDurationMinutes", function(duration) {
+          pump.doCycle(duration.value, null);
+        });
+
+      };
+    });
+
+  }, 1000);
+
+
+});
---
+++ node_modules/iot-thing/README.md
@@ -0,0 +1,3 @@
+[![NPM](https://nodei.co/npm/iot-thing.png)](https://nodei.co/npm/iot-thing/)
+
+This is the iot-thing client from my other project, [que-iot](//github.con/1egoman/que-iot)
